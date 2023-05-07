9d77a8eb3cefcf381dec88f1fb2c609838e6b704
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-05-06 20:08:05 -0700

    Publishing

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -36,5 +36,19 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(17)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/nosadnile/serverhelper")
+
+            credentials {
+                username = System.getProperty("github.user") ?: System.getenv("GITHUB_ACTOR")
+                password = System.getProperty("github.token") ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
